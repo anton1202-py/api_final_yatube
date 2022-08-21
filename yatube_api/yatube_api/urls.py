590310f7702c40b167aedecdbe5459d06f8aeb4b
Антон Кузнецов <anton1202@yandex.ru>
2022-08-21 10:28:53 +0300

    проект на первое ревью

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,11 +1,11 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-
 from rest_framework import permissions
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
