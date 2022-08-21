bfbe160563cff59535e9cfaa112d9593af0a17e0
Антон Кузнецов <anton1202@yandex.ru>
2022-08-21 10:20:16 +0300

    Проект на 1-ое ревью

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -10,9 +15,26 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    # Djoser создаст набор необходимых эндпоинтов.
-    # базовые, для управления пользователями в Django:
     path('api/v1/', include('djoser.urls')),
-    # JWT-эндпоинты, для управления JWT-токенами:
     path('api/v1/', include('djoser.urls.jwt')),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Yatube API",
+        default_version='v1',
+        description="Документация для приложения Yatube API",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
